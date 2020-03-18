aa09836322e89c6e64b119fbf5509cc09975d114
Eric Sanchez <eric.sanchez@equinox.com>
2020-03-18 12:53:17 -0400

    Added notifications.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,7 @@
 var deferredPrompt;
+var enableNotoficationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -21,3 +24,44 @@ window.addEventListener("beforeinstallprompt", function(event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification() {
+  if ("serviceWorker" in navigator) {
+    var options = {
+      body: "You successfully subscribe to our notification service",
+      icon: "/src/images/icons/app-icon-96x96.png",
+      image: "/src/images/sf-boat.jpg",
+      dir: "ltr",
+      lang: "en-US",
+      vibrate: [100, 50, 200],
+      badge: "/src/images/icons/app-icon-96x96.png",
+      tag: "confirm-notification"
+    };
+    navigator.serviceWorker.ready.then(swreg => {
+      swreg.showNotification("Successfully subscribed FROM SW", options);
+    });
+  }
+
+  // new Notification("Successfully subscribed", options);
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(result => {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted");
+    } else {
+      displayConfirmNotification();
+    }
+  });
+}
+
+if ("Notification" in window) {
+  for (var i = 0; i < enableNotoficationsButtons.length; i++) {
+    enableNotoficationsButtons[i].style.display = "inline-block";
+    enableNotoficationsButtons[i].addEventListener(
+      "click",
+      askForNotificationPermission
+    );
+  }
+}
